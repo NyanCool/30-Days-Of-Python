ce8b034441f694a444de9cceadb14752a0995f09
NicolasZhang <nzhang@ipsosenso.com>
2026-09-02 09:19:24 +0200

    Exercices day 2

---
+++ day_2/variables.py
@@ -0,0 +1,36 @@
+# Day 2: 30 Days of python programming
+
+# Exercice 1
+first_name = 'first name'
+last_name = 'last name'
+full_name = 'full name'
+country = 'country'
+city = 'city'
+age = 'age'
+year = 'year'
+is_married = False
+is_true = True
+is_light_on = True
+test_1, test_2 = 'test_1', 'test_2'
+
+# Exercise 2
+
+print(type(is_married))
+print(type(city))
+print(len(first_name))
+print(len(first_name), len(last_name))
+num_one = 5
+num_two = 4
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one ** num_two
+floor_division = num_one // num_two
+area_of_circle = 3.14 * (30**2)
+circum_of_circle = 3.14 * 30
+
+# Input section
+
+help('keywords')
